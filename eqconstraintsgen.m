0fa6e3664dd591351cdba9b2cfeccfc4cfb96ca3
PetarVelchev <p.a.velchev@student.tudelft.nl>
2024-03-19 14:11:22 +0100

    First MPC worky

--- eqconstraintsgen.m
+++ eqconstraintsgen.m
@@ -1,6 +1,6 @@
-function eqconstraints=eqconstraintsgen(LTI,dim,dtilde)
+function eqconstraints=eqconstraintsgen(LTI,dim)
 
 eqconstraints.A=[eye(dim.nx)-LTI.A -LTI.B; LTI.C zeros(dim.ny,dim.nu)];
-eqconstraints.b=[LTI.Bd*dtilde; LTI.yref-LTI.Cd*dtilde];
+eqconstraints.b=[zeros(dim.nx,1); LTI.yref];
 
 end
